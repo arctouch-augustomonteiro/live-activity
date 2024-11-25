5d6b246d718e1d780f76db0aeeab5a9da9b2b5f0
Augusto Monteiro <augusto.monteiro@arctouch.com>
2024-11-25 20:31:58 -0300

    Create widget target

---
+++ CounterWidget/CounterWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CounterWidgetBundle.swift
+//  CounterWidget
+//
+//  Created by Augusto Monteiro on 25/11/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CounterWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CounterWidget()
+    }
+}
